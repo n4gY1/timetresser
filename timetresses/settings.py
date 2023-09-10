eac6964fbb8cc1c1ecefec4bcc990a166c20ac83
nagy- <hybase@outlook.hu>
2023-09-10 20:37:31 +0200

    procfile2322552faszom

--- timetresses/settings.py
+++ timetresses/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)wo^pxcqp)z&5!u@h*o0mx#(7e&(^u92!uqm1!ns&w(i%&@x&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
