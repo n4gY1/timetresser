8a83c3e2436ada1828f4bbb5d51f86cf411f85bd
nagy- <hybase@outlook.hu>
2023-09-11 19:11:32 +0200

    media iss

--- timetresses/settings.py
+++ timetresses/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/app/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
