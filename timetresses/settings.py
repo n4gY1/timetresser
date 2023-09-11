1e987e25ce3e824e25e1f749e24538a908933626
nagy- <hybase@outlook.hu>
2023-09-11 18:49:29 +0200

    media dird

--- timetresses/settings.py
+++ timetresses/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
