81a29c91bb1160756da9c539c932ddb18677c97c
nagy- <hybase@outlook.hu>
2025-07-04 22:02:30 +0200

    deploy 1 - staticfiles 2

--- timetresses/settings.py
+++ timetresses/settings.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_ROOT = "staticfiles/"
+
 #STATIC_ROOT = "/home/nagy-webpages/timetresser/publishstaticfiles" #hogy ennek van e köze hozzá????
 
 
